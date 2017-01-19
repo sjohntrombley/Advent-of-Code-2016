36e49ff7de5481720f94776b50cf5cabf6d1425e
Sandrow John Trombley <s.john.trombley@gmail.com>
2017-01-19 13:02:20 -0500

    Finished commenting day 1 solution

--- 01/main.hs
+++ 01/main.hs
@@ -9,7 +9,7 @@ data Orientation = North | East | South | West  deriving Show
 data Position = Position Orientation (Int,Int)  deriving Show
 
 -- Binding:       main
--- Description:   Gets program arguemnts and program name. The first argument
+-- Description:   Gets program arguments and program name. The first argument
 --    is run mode (either '-1', '-2', or '-12'. The second is the name of the
 --    file containing directions. The String read from the input file is parsed
 --    by the parse function, then either solve1, solve2, or both is called on
@@ -33,7 +33,7 @@ main = do
     else error "Invalid run mode."
 
 -- Function:      parse
--- Arguemnt:      Input string of directions
+-- Argument:      Input string of directions
 -- Description:   Takes an input string as an argument, and returns a list of
 --    Directions. It simply wraps parse' and passes it initial parameters of an
 --    empty partial token and an empty partial direction list.
@@ -53,7 +53,7 @@ parse = parse' "" []
 --    mkDirection. This Direction is added to the head of pdl. All non-token
 --    characters are stripped from the head of cs, and parse' is called
 --    recursively. If cs is empty and pt is empty, then cs had trailing
---    non-tken characters, so pdl is reversed and returned. If pt is nonempty,
+--    non-token characters, so pdl is reversed and returned. If pt is nonempty,
 --    cs had no trailing non-token characters, and the last token is parsed and
 --    added to the head of pdl, which is then reversed and returned.
 parse' :: String -> [Direction] -> String -> [Direction]
@@ -67,32 +67,56 @@ parse' pt pdl (c:cs) = if isTokenC c
 
 isTokenC = (`elem` "1234567890LR")
 
--- Turns a single token into a direction object
 mkDirection :: String -> Direction
 mkDirection (d:l) = case d of
   'L'         -> Left  $ read l
   'R'         -> Right $ read l
   otherwise   -> error "Error in input file.\n"
 
--- Solver for the first part of the problem. Simply folds the direction list
--- using move from the initial position and orientation, then calculates the
--- distance to the result.
+-- Function:      solve1
+-- Arguments:
+--    * (dirs::[Direction) the list of directions returned by parse.
+-- Description:   The solution is calculated by simply calling foldl with move
+--    as its functional argument on dirs. The initial value for the fold is the 
+--    initial coordinates and orientation, namely (0,0) and North, 
+--    respectively. The final location is extracted from the fold result, and
+--    the absolute values of the coordinates are added.
 solve1 :: [Direction] -> Int
 solve1 dirs = let
     Position _ (x,y) = foldl move (Position North (0,0)) dirs
   in (abs x)+(abs y)
 
--- solve2 is the solver function for the second part of the problem. It wraps
--- the recursive function solve2' which does the work.
+-- Function:      solve2
+-- Arguments:
+--    * (dirs::[Direction]) The list of directions returned by parse
+-- Description:   Takes the list of directions returned by parse and returns
+--    the distance to the first point visited twice. It wraps solve2', passing
+--    initial arguments of (Position North (0,0)) for the initial position, a
+--    list containing only (0,0) for the history (since the current position is
+--    expected to be in the history by solve2'), and dirs for the direction
+--    list.
 solve2 :: [Direction] -> Int
 solve2 = solve2' (Position North (0,0)) [(0,0)]
 
--- solve2' takes a position, a history of past locations, and a list of
--- directions, and returns the distance to the first location visited twice. It
--- does this by calculating all of the points between its initial position and
--- its final position after the next move in the direction list. q
+-- Function:      solve2'
+-- Arguments:
+--    * (op::Position) the current position along the path.
+--    * (h::[(Int,Int)]) the history of locations already visited. Should
+--      include the location contained in op.
+--    * (ds::[Direction]) the remaining list of directions.
+-- Description:   solve2' first checks if the remaining direction list is
+--    empty. If it is, no location is visited twice, and so an error is
+--    produced. If the direction list is non-empty, the endpoint of the next
+--    direction is determined. A list of locations between op and the
+--    destination location, in order of visiting and including the destination,
+--    but excluding the original location, is returned by iterate. getSolFromIL
+--    returns Nothing if no location is visited twice yet, or Just i, where i 
+--    is the distance to the location, if one is. If it is Nothing, the
+--    interpolated list is prepended to the history, and solve2' is called
+--    recursively with the new Position, new history, and the tail of the
+--    direction list.
 solve2' :: Position -> [(Int,Int)] -> [Direction] -> Int
-solve2' _ _ [] = error "No location visited twiced."
+solve2' _ _ [] = error "No location visited twice.\n"
 solve2' op@(Position _ oloc) h (d:ds) = let
     np@(Position _ loc@(x,y)) = move op d
     il = interpolate oloc loc
